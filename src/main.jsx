075d832d446dafc807742996a2be6213444ee77a
Bryan Miller <b.miller@live.co.uk>
2023-09-28 17:30:22 +0100

    Switch to hash router so gh-pages works

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
@@ -15,7 +15,7 @@ import './index.css'
 import { ProductsRoute } from './pages/Products';
 import { OrderRoute } from './pages/Order';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <Navbar/>,
